61aad8c31c99decdfefb10d81b06f27baaaf97da
nairit132 <nairithmajmundar@gmail.com>
2025-04-14 19:20:42 +0100

    Add files via upload

---
+++ task two.py
@@ -0,0 +1,6 @@
+string = input(('Please give your own string :'))
+string2 =  ("")
+for i in string :
+    string2 = i + string2
+print("\n The original sting ", string)
+print("The reversed string ", string2)
